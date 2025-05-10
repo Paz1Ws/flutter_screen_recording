89e65b8db14f5f0473debf4537a7cba7587e2774
Christopher Paz León <142619752+Paz1Ws@users.noreply.github.com>
2025-05-10 10:03:11 -0500

    Update ForegroundService.kt

--- flutter_screen_recording/android/src/main/kotlin/com/isvisoft/flutter_screen_recording/ForegroundService.kt
+++ flutter_screen_recording/android/src/main/kotlin/com/isvisoft/flutter_screen_recording/ForegroundService.kt
@@ -119,7 +119,6 @@ class ForegroundService : Service() {
             .setContentText(message)
             .build()
 
-
         startForeground(1, notification)
         println("-------------------------- startForegroundServiceWithNotification")
     }
